ee4197d29c929b8262f1c7c7f80d45317bce19d9
wilversings <aiordachioaei.marius@gmail.com>
2016-08-14 22:49:28 +0300

    Settings
    Implemented the set function of settings + syncing

--- LSMachine/Utils/Settings.cs
+++ LSMachine/Utils/Settings.cs
@@ -10,6 +10,8 @@ namespace LSMachine.Utils {
 		private static XDocument SettingsXml;
 		private static string SettingsPath = "../../app.config";
 
+		public static bool AutoSync { get; set; } = false;
+
 		// <summary>
 		// Loads the settings from the SettingsPath specified variable
 		// </summary>
@@ -21,14 +23,37 @@ namespace LSMachine.Utils {
 		// Gets the setting with the specified name
 		// </summary>
 		// <param name = "Setting"> The name of the setting </param>
-		public static string GetSetting (string Setting) {
+		public static string GetSetting (string Name) {
 
 			IEnumerable<string> settingList = 
 				from s in SettingsXml.Element("configuration").Elements()
-				where s.Attribute("name").Value == Setting
+				where s.Attribute("name").Value == Name
 				select s.Attribute("value").Value;
 
 			return settingList.FirstOrDefault();
+
+		}
+
+		public static void SetSetting (string Name, string Value) {
+
+			var configurationNode = SettingsXml.Element("configuration");
+
+			IEnumerable<XElement> settingList = 
+				from s in configurationNode.Elements()
+				where s.Attribute("name").Value == Name
+				select s;
+
+			if (settingList.Count() != 0) {
+				settingList.First().SetAttributeValue("value", Value);
+			} else {
+				var newXmlNode = new XElement("setting");
+				newXmlNode.SetAttributeValue("name", Name);
+				newXmlNode.SetAttributeValue("value", Value);
+				configurationNode.Add(newXmlNode);
+			}
+			if (AutoSync) {
+				SettingsXml.Save(SettingsPath);
+			}
 
 		}
 			
