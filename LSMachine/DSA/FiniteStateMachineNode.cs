366381eb38422f069ed86d49fcfa74e826dea928
wilversings <aiordachioaei.marius@gmail.com>
2016-08-14 23:15:20 +0300

    FSM Node
    Implemented the class basics

---
+++ LSMachine/DSA/FiniteStateMachineNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LSMachine
+{
+	public abstract partial class FiniteStateMachine <T>
+	{
+		public class Node {
+
+			public ICollection<Node> NextStates { get; set; }
+			public bool IsFinishState { get; set; }
+			public T Data { get; set; }
+
+			public Node Associate (Node Second) {
+
+				NextStates.Add(Second);
+
+				return this;
+			}
+
+		}
+	}
+}
+
