890bef5553c957b2496d33b14ab5d861aea7e9b1
wilversings <aiordachioaei.marius@gmail.com>
2016-08-14 22:32:09 +0300

    Settings
    Implemented the settings load function

---
+++ LSMachine/Utils/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace LSMachine.Utils {
+	public static class Settings {
+
+		private static XDocument SettingsXml;
+		private static string SettingsPath = "../../app.config";
+
+		// <summary>
+		// Loads the settings from the SettingsPath specified variable
+		// </summary>
+		public static void Load () {
+			SettingsXml = XDocument.Load(SettingsPath);
+		}
+
+		// <summary>
+		// Gets the setting with the specified name
+		// </summary>
+		// <param name = "Setting"> The name of the setting </param>
+		public static string GetSetting (string Setting) {
+
+			IEnumerable<string> settingList = 
+				from s in SettingsXml.Element("configuration").Elements()
+				where s.Attribute("name").Value == Setting
+				select s.Attribute("value").Value;
+
+			return settingList.FirstOrDefault();
+
+		}
+			
+	}
+}
+
