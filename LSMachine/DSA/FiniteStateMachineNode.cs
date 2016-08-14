ab8c54806d41f826e3d96924e0992b85d19ba89d
wilversings <aiordachioaei.marius@gmail.com>
2016-08-14 23:46:45 +0300

    FSM Node
    Implemented constructor and made the class iterable

--- LSMachine/DSA/FiniteStateMachineNode.cs
+++ LSMachine/DSA/FiniteStateMachineNode.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 
 namespace LSMachine
 {
-	public abstract partial class FiniteStateMachine <T>
-	{
-		public class Node {
+	public abstract partial class FiniteStateMachine <T> {
+		public partial class Node :
+			IEnumerable<Node> {
 
 			public ICollection<Node> NextStates { get; set; }
 			public bool IsFinishState { get; set; }
 			public T Data { get; set; }
+
+			// <summary>
+			// Associates `this` with `Second` node in the finite state machine
+			// </summary>
+			// <param name="Second"> The node to asociate `this` with </param>
+
+			public Node () {
+				NextStates = new List<Node> ();
+			}
 
 			public Node Associate (Node Second) {
 
@@ -18,6 +27,15 @@ namespace LSMachine
 				return this;
 			}
 
+			public IEnumerator<Node> GetEnumerator ()
+			{
+				return NextStates.GetEnumerator();
+			}
+
+			System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator ()
+			{
+				return NextStates.GetEnumerator();
+			}
 		}
 	}
 }
