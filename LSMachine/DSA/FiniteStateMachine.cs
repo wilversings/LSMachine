ade5d5ee027cdf8a4ba1f9afd405e8b8b095310f
wilversings <aiordachioaei.marius@gmail.com>
2016-08-15 00:14:43 +0300

    Finite State Machine
    Implemented the abstract class

---
+++ LSMachine/DSA/FiniteStateMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LSMachine
+{
+	public abstract partial class FiniteStateMachine <T> {
+
+		protected ICollection<State> States;
+
+		public State StartState { get; set; }
+		public State CurrentState { get; set; }
+
+		public State CreateNewState () {
+			var newState = new State(this);
+			States.Add(newState);
+			return new State(this);
+		}
+
+		public FiniteStateMachine () {
+			var startState = CreateNewState();
+			var endState = CreateNewState();
+
+			StartState = startState;
+			endState.IsFinishState = true;
+
+		}
+
+		public IEnumerable<State> FinishStates () {
+			return from s in States
+			       where s.IsFinishState == true
+			       select s;
+		}
+
+		public abstract State GoToNextState ();
+
+	}
+}
+
