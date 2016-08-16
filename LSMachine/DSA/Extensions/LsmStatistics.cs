fc93af001721369c8b68112351398965d71116b4
wilversings <aiordachioaei.marius@gmail.com>
2016-08-16 19:46:38 +0300

    Statistics
    Added LSM Statistics and implemented the redundancy method

---
+++ LSMachine/DSA/Extensions/LsmStatistics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LSMachine {
+	public static class LsmStatistics {
+
+		public static int Redundancy<TValue>
+		(this DialectModel<TValue> Lsm, int CasesRan) {
+
+			string data = System.IO.File.ReadAllText(Utils.Settings.GetSetting("RawDataPath"));
+			int redundancy = 0;
+
+			while (CasesRan-- != 0) {
+				string generated = string.Join(" ", Lsm.Generate());
+				if (data.Contains(generated)) {
+					++redundancy;
+				}
+			}
+
+			return redundancy;
+
+		}
+
+	}
+}
+
