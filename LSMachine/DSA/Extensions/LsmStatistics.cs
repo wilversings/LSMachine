2602e787e06a32bf368dee838b21bbbbf6e9fd8b
wilversings <aiordachioaei.marius@gmail.com>
2016-08-17 22:31:41 +0300

    Statistics
    Added IsRedundant method on Lsm statistics

--- LSMachine/DSA/Extensions/LsmStatistics.cs
+++ LSMachine/DSA/Extensions/LsmStatistics.cs
@@ -1,22 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LSMachine {
 	public static class LsmStatistics {
 
+		private static List<string> data = null;
+
 		public static int Redundancy<TValue>
 		(this DialectModel<TValue> Lsm, int CasesRan) {
 
-			string data = System.IO.File.ReadAllText(Utils.Settings.GetSetting("RawDataPath"));
 			int redundancy = 0;
 
 			while (CasesRan-- != 0) {
-				string generated = string.Join(" ", Lsm.Generate());
-				if (data.Contains(generated)) {
+				if (IsRendundant(Lsm.Generate())) {
 					++redundancy;
 				}
 			}
 
 			return redundancy;
+
+		}
+
+		public static bool IsRendundant (ICollection<string> output) {
+
+			if (data == null)
+				data = System.IO.File.ReadAllLines(Utils.Settings.GetSetting("RawDataPath")).ToList();
+
+			string outputString = string.Join(" ", output);
+			foreach (string line in data) {
+				if (line.Contains(outputString)) {
+					return true;
+				}
+			}
+
+			return false;
 
 		}
 
