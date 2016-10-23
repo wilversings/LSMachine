679dfab1ac4d5040559388ae0710a1fdc50d4df0
Aiordăchioaei Marius <aiordachioaei.marius@gmail.com>
2016-10-23 18:42:11 +0300

    Implementation
    Implemented the Word optimal distance using Levenshtein distance

--- LSMachine/Domain/Word.cs
+++ LSMachine/Domain/Word.cs
@@ -11,8 +11,30 @@ namespace LSMachine {
 			Reprezentation = Word;
 		}
 			
-		public static int operator - (Word First, Word Second) {
-			throw new NotImplementedException ();
+		public static uint operator - (Word First, Word Second) {
+
+			var dTable = new uint[First.Reprezentation.Length, Second.Reprezentation.Length];
+			string first = First.Reprezentation,
+				   second = Second.Reprezentation;
+			int fLen = First.Reprezentation.Length, 
+				 sLen = Second.Reprezentation.Length;
+
+			dTable [0, 0] = second [0] == first [0] ? 0u : 1u;
+			for (int i = 1; i < fLen; ++i)
+				dTable [i, 0] = dTable [i - 1, 0] + (second[0] == first[i] ? 0u : 1u);
+			for (int i = 1; i < sLen; ++i)
+				dTable [0, i] = dTable [0, i - 1] +  (first[0] == second[i] ? 0u : 1u);
+
+			for (int i = 1; i < fLen; ++i)
+				for (int j = 1; j < sLen; ++j)
+					if (first [i] == second [j])
+						dTable [i, j] = dTable [i - 1, j - 1];
+					else 
+						dTable [i, j] = Math.Min (dTable [i - 1, j] + 1, dTable [i, j - 1] + 1);
+					
+
+			return dTable [fLen - 1, sLen - 1];
+
 		}
 
 		public static Word operator + (Word First, Word Second) {
