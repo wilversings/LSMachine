bb35735747b3d9d6c46ec6592183dd40a0921c63
Aiordăchioaei Marius <aiordachioaei.marius@gmail.com>
2016-10-23 17:54:11 +0300

    Added the Word entity

---
+++ LSMachine/Domain/Word.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LSMachine {
+	
+	public class Word {
+
+		private string Reprezentation { get; set; }
+		// TODO Culture info field
+
+		public Word (string Word) {
+			Reprezentation = Word;
+		}
+			
+		public static int operator - (Word First, Word Second) {
+			throw new NotImplementedException ();
+		}
+
+		public static Word operator + (Word First, Word Second) {
+			throw new NotImplementedException ();
+		}
+
+	}
+
+}
+
